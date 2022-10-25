c406185302f75f88c9a34c299a0c9c53cb4c8c75
tang <tangkanis@gmail.com>
2022-10-26 00:08:13 +0700

    add space

--- main.tf
+++ main.tf
@@ -86,7 +86,7 @@ resource "google_compute_firewall" "flask" {
 
 // A variable for extracting the external IP address of the VM
 output "Web-server-URL" {
-  value = join("",["http://",google_compute_instance.default.network_interface.0.access_config.0.nat_ip,":5000"])
+  value = join(" ",["http://",google_compute_instance.default.network_interface.0.access_config.0.nat_ip,":5000"])
 }
 # [END vpc_flask_quickstart_5000_fw]
 
